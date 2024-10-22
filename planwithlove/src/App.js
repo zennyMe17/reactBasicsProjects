609a0f3d005c36abc54080d0ebc64a224b23c030
zennyMe17 <gowad.hemanth.1718@gmail.com>
2024-10-22 18:12:39 +0530

    Project-2

--- planwithlove/src/App.js
+++ planwithlove/src/App.js
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
-import "./App.css";
 import data from './data.js';
-import Tours from './components/Tours.js'
-function App() {
-  const [tours,setTours] = useState(data);
-  function removeTour(id){
+import Tours from './Component/Tours';
+
+const App = () => {
+  const [tours, setTours] = useState(data);
+  
+  function removeTour(id) {
     const newTours = tours.filter(tour => tour.id !== id);
     setTours(newTours);
   }
+
+  if (tours.length === 0) {
+    return (
+      <div className="refresh">
+        <h2>No Tours Left</h2>
+        <button className="btnWhite" onClick={() => setTours(data)}>Refresh</button>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <Tours tours={tours} removeTour = {removeTour}></Tours>
+    <div className="app">
+      <Tours tours={tours} removeTours={removeTour} />
     </div>
-  );
-}
+  )
+
+};
 
 export default App;
