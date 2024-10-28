92c9b17550494d36a55dec337ae7004b8d67d54e
zennyMe17 <gowad.hemanth.1718@gmail.com>
2024-10-28 17:35:49 +0530

    Top-Course-Starter

--- top-course-starter/src/components/Card.jsx
+++ top-course-starter/src/components/Card.jsx
@@ -1,28 +1,47 @@
 import React from "react";
 import { FcLike, FcLikePlaceholder } from 'react-icons/fc';
+import { toast } from 'react-toastify'; 
 
-const Card = (props)=>{
-    return(
+const Card = (props) => {
+    let course = props.course;
+    let likedCourses = props.likedCourses;
+    let setLikedCourses = props.setLikedCourses;
+
+    function clickHandler() {
+        console.log("Inside clickHandler"); 
+        console.log("Current liked courses:", likedCourses); 
+
+        if (likedCourses.includes(course.id)) {
+            setLikedCourses((prev) => prev.filter((cid) => cid !== course.id));
+            toast.warning("Liked Removed");
+        } else {
+            setLikedCourses((prev) => [...prev, course.id]);
+            toast.success("Liked Successfully");
+        }
+    }
+
+    console.log("Rendering card for:", course.title); 
+
+    return (
         <div className='bg-bgDark bg-opacity-80 w-[300px] rounded-md overflow-hidden'>
-             <div className='relative '>
-                <img src={props.course.image.url} alt="Course Image" className='' />
+            <div className='relative'>
+                <img src={course.image.url} alt="Course Image" />
                 <div className='rounded-full w-[40px] h-[40px] bg-white absolute right-2 bottom-[-12px] grid place-items-center'>
-                    <button>
-                        <FcLike fontSize="1.75rem" />
+                    <button onClick={() => { console.log("Button clicked!"); clickHandler(); }}>
+                        {
+                            !likedCourses.includes(course.id) 
+                                ? <FcLikePlaceholder fontSize="1.75rem" /> 
+                                : <FcLike fontSize="1.75rem" />
+                        }
                     </button>
                 </div>
-
-            </div> 
+            </div>
             <div className="p-4">
-                <p
-                className='text-white text-lg font-semibold leading-6'
-                >{props.course.title}</p>
-                <p 
-                className='mt-2 text-white'
-                >{props.course.description}</p>
+                <p className='text-white text-lg font-semibold leading-6'>{course.title}</p>
+                <p className='mt-2 text-white'>{course.description}</p>
             </div>
         </div>
-    )
+    );
 }
 
-export default Card
+export default Card;
